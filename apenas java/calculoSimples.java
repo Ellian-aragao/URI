d77c12e87ea37e7090cc4a0bf2267d73a1fc6727
ellian-aragao <ellian.aragao@gmail.com>
2020-01-10 09:18:23 -0300

    alterando nome das classes para condizer com arquivo

--- apenas java/calculoSimples.java
+++ apenas java/calculoSimples.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Main {
+public class calculoSimples {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double total = 0;
